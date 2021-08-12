e6223ea2446d282b3c702c7c6058dc9d809f8761
gunjou <gjoefandi@gmail.com>
2021-08-12 09:12:48 +0800

    menambahkan direktory gj

---
+++ gj-learning/gj-komentar.py
@@ -0,0 +1,15 @@
+# Untuk menambahkan komentar dapat menggunakan tanda '#'
+# Atau pada text editor menggunakan 'ctrl + /'
+
+# Tanda # hanya untuk single line comment
+# apabila menginginkan comment multi line dapat ditulis """comment disini"""
+
+# Contoh Multi line comment
+""" Berikut adalah contoh dari multi line komen,
+anda dapat menuliskan dimanapun tidak hanya untuk satu line
+"""
+
+print("Hello, World") # ini termasuk komen disamping sintax
+
+# Mencetak Integer
+print(128)
