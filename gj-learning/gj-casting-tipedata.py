5775ca61f93a05df82a11ab6e79eef4dacd5160a
gunjou <gjoefandi@gmail.com>
2021-08-12 11:43:24 +0800

    menambahkan file gj-casting-tipedata.py

---
+++ gj-learning/gj-casting-tipedata.py
@@ -0,0 +1,15 @@
+# Casting tipe data yaitu merubah tipe data ke tipe yang lain
+
+data_int = 12
+print(data_int, " bertipe : ", type(data_int))
+
+# merubah tipe data int diatas
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+
+print(data_float, " bertipe : ", type(data_float))
+print(data_str, " bertipe : ", type(data_str))
+print(data_bool, " bertipe : ", type(data_bool))
+
+print("Hal ini berlaku untuk sebaliknya")
